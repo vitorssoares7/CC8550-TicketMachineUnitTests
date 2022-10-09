296d54ef985b4dc1bc20a1c98c711bb8d458a5f6
Vitor Soares <57197419+vitorssoares7@users.noreply.github.com>
2022-10-09 17:29:35 -0300

    Update TrocoTest.java

--- ticketmachine/tests/TrocoTest.java
+++ ticketmachine/tests/TrocoTest.java
@@ -6,14 +6,14 @@ import org.junit.Test;
 
 import ticketmachine.core.Troco;
 public class TrocoTest {
-    @Test(timeout=2000)
+    
     public void TrocoContructor() {
         int valor = 30;
         Troco troco = new Troco(valor);
         Assert.assertNotNull(troco);
     }
 
-    @Test
+    
     public void TrocoIteratorConstructor() {
         int valor = 20;
         Troco troco = new Troco(valor);
